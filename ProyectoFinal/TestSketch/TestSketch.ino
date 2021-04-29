3aaada8af0eadd232421c56f503093ae6a0201cc
jorgehurt <hurtadoj542@gmail.com>
2021-04-29 04:36:43 -0600

    ProyectoFinalCasi

---
+++ ProyectoFinal/TestSketch/TestSketch.ino
@@ -0,0 +1,392 @@
+
+#include <Adafruit_GFX.h>
+#include <UTFTGLUE.h>              //use GLUE class and constructor
+UTFTGLUE myGLCD(0x5408,A2,A1,A3,A4,A0); //all dummy args
+
+
+int timertemp = 300;
+int contadorJ1 =6;
+int contadorJ2 =3;
+int Ballx=random(100,200);
+int Bally=random(80,120);
+int BallSpeed = 10;
+int BallRamp = 2;
+int ResBally= 0;
+int ResBallx= 0;
+
+//Bar Game Position
+const int BarLenght = 60;
+//BarPos
+int BarPosUpRight = 170;
+int BarPosUpLeft = 10;
+int BarPosDownRight = BarPosUpRight + BarLenght;
+int BarPosDownLeft = BarPosUpLeft + BarLenght;
+
+
+//Move Right Bar CTE
+const int BarSteps = 10;//Bar Speed Movement
+int ButtonUpRightBar = 0;
+int ButtonDownRightBar = 0;
+int RightBarCounter = BarPosUpRight;
+int LeftBarCounter = BarPosUpLeft;
+
+//Move Right Bar CTE
+int ButtonUpLeftBar = 0;
+int ButtonDownLeftBar = 0;
+
+byte SendSerial = 0b00000000;
+
+
+
+
+void setup()
+{
+  randomSeed(analogRead(0));
+  Serial.begin(9600);
+// Setup the LCD
+  myGLCD.InitLCD();
+  myGLCD.setFont(BigFont);
+  myGLCD.fillScr(0, 0, 0);  
+
+}
+
+void loop()
+{
+  Serial.write(1);
+  SendSerial=Serial.read();
+  ButtonUpRightBar = bitRead(SendSerial,1);
+  ButtonDownRightBar = bitRead(SendSerial,2);
+  ButtonUpLeftBar = bitRead(SendSerial,3);
+  ButtonDownLeftBar = bitRead(SendSerial,4);
+
+  //InitScreen ();
+
+  MoveBall ();
+  delay(100);
+  MoveRightBar ();
+  delay(100);
+  MoveLeftBar ();
+  delay(100);
+
+/*
+
+  
+  //int buf[218];
+  //Instructions:
+  //myGLCD.fillRoundRect(0, 0, 315, 235); //Full Display Recomended Size
+  //myGLCD.setColor(0, 255, 255); 
+  //myGLCD.print("few seconds...", CENTER, 101);
+  //myGLCD.setColor(0, 255, 0);
+  //myGLCD.setBackColor(0, 0, 255);
+  //myGLCD.print("Runtime: (msecs)", CENTER, 146);
+  //myGLCD.printNumI(millis(), CENTER, 161);
+  
+  
+  
+  MoveBall ();
+  ButtonUpRightBar = 1;
+  MoveRightBar ();
+  ButtonUpRightBar = 0;
+  ButtonDownLeftBar = 1;
+  MoveLeftBar ();
+  ButtonDownLeftBar = 0;  
+  delay(1000);
+
+
+const unsigned char PROGMEM Bola[32] = {   
+  0x1E, 0x00, //    ####   
+  0x3F, 0x00, //   ######  
+  0x3F, 0x80, //   ####### 
+  0x7F, 0x80, //  ######## 
+  0x7F, 0x80, //  ######## 
+  0xF3, 0xC0, // ####  ####
+  0xED, 0xC0, // ### ## ###
+  0xE3, 0xC0, // ###   ####
+  0xE9, 0xC0, // ### #  ###
+  0xF5, 0xC0, // #### # ###
+  0xF3, 0xC0, // ####  ####
+  0x7F, 0x80, //  ######## 
+  0x7F, 0x80, //  ######## 
+  0x7F, 0x80, //  ######## 
+  0x3F, 0x00, //   ######  
+  0x1E, 0x00, //    ####
+};
+
+//myGLCD.drawBitmap(100,100,20,20,Bola) ;
+
+*/
+
+
+
+
+
+}
+
+void InitScreen ()
+{
+  //Pantalla de Inicio
+  myGLCD.fillScr(100, 250, 100);
+  myGLCD.setColor(255, 0, 0);
+  myGLCD.fillRoundRect(57, 150, 257,210);
+  myGLCD.setColor(0, 0, 0);
+  myGLCD.setBackColor(150, 150, 150);
+  myGLCD.fillRoundRect(0, 20, 318, 55);
+  myGLCD.setColor(255,255, 255);
+  myGLCD.print("Pong!", CENTER, 32);
+  myGLCD.setBackColor(255, 10, 0);
+  myGLCD.setColor(0, 100, 255);
+  myGLCD.print("Inicio", CENTER, 180);
+  myGLCD.setFont(BigFont);
+  myGLCD.setBackColor(0, 10 , 255);
+  myGLCD.setColor(255, 100, 0);
+  myGLCD.print("Click any key", CENTER, 105);
+  myGLCD.print("to Start", CENTER, 125);
+  myGLCD.setFont(BigFont);
+}
+
+void RightBar ()
+{
+  //Pantalla de Juego Barra derecha
+  //myGLCD.fillScr(0, 0, 0);
+  myGLCD.setColor(255, 255, 255);
+  myGLCD.fillRoundRect(300, BarPosUpRight, 310, BarPosDownRight);//70
+}
+
+void ClearRightBar ()
+{
+  //Pantalla de Juego Barra derecha
+  //myGLCD.fillScr(0, 0, 0);
+  myGLCD.setColor(0, 0, 0);
+  myGLCD.fillRoundRect(300, BarPosUpRight, 310, BarPosDownRight);//70
+}
+
+void MoveRightBar (){
+  Serial.write(1);
+  SendSerial=Serial.read();
+  ButtonUpRightBar = bitRead(SendSerial,1);
+  ButtonDownRightBar = bitRead(SendSerial,2);
+  ButtonUpLeftBar = bitRead(SendSerial,3);
+  ButtonDownLeftBar = bitRead(SendSerial,4);
+  ClearRightBar ();
+  if (ButtonUpRightBar == 1){
+    if (RightBarCounter > 10){
+      RightBarCounter = RightBarCounter - BarSteps;
+      BarPosUpRight = RightBarCounter;
+      BarPosDownRight = BarPosUpRight + BarLenght;
+      }
+      RightBar (); 
+      return;
+    }
+  if (ButtonDownRightBar == 1){
+    if (RightBarCounter < 170){
+      RightBarCounter = RightBarCounter + BarSteps;
+      BarPosUpRight = RightBarCounter;
+      BarPosDownRight = BarPosUpRight + BarLenght; 
+      }
+      RightBar (); 
+      return;    
+    }
+  
+  return;
+  }
+
+void LeftBar ()
+{
+  //Pantalla de Juego Barra Izquierda\
+  //myGLCD.fillScr(0, 0, 0);
+  myGLCD.setColor(255, 255, 255);
+  myGLCD.fillRoundRect(10, BarPosUpLeft, 20, BarPosDownLeft);//170
+}
+
+void ClearLeftBar ()
+{
+  //Pantalla de Juego Barra derecha
+  //myGLCD.fillScr(0, 0, 0);
+  myGLCD.setColor(0, 0, 0);
+  myGLCD.fillRoundRect(10, BarPosUpLeft, 20, BarPosDownLeft);//70
+}
+
+void MoveLeftBar (){
+  Serial.write(1);
+  SendSerial=Serial.read();
+  ButtonUpRightBar = bitRead(SendSerial,1);
+  ButtonDownRightBar = bitRead(SendSerial,2);
+  ButtonUpLeftBar = bitRead(SendSerial,3);
+  ButtonDownLeftBar = bitRead(SendSerial,4);
+  ClearLeftBar ();
+  if (ButtonUpLeftBar == 1){
+    if (LeftBarCounter > 10){
+      LeftBarCounter = LeftBarCounter - BarSteps;
+      BarPosUpLeft = LeftBarCounter;
+      BarPosDownLeft = BarPosUpLeft + BarLenght;
+      }
+      LeftBar (); 
+      return;
+    }
+  if (ButtonDownLeftBar == 1){
+    if (LeftBarCounter < 170){
+      LeftBarCounter = LeftBarCounter + BarSteps;
+      BarPosUpLeft = LeftBarCounter;
+      BarPosDownLeft = BarPosUpLeft + BarLenght; 
+      }
+      LeftBar (); 
+      return;    
+    }
+  
+  return;
+  }
+
+void Ball ()
+{
+  //Pelota
+  //myGLCD.fillScr(0, 0, 0);
+  myGLCD.setColor(255, 255, 255);
+  myGLCD.fillCircle(Ballx,Bally, 5);
+}
+
+void ClearBall ()
+{
+  //Pelota
+  //myGLCD.fillScr(0, 0, 0);
+  myGLCD.setColor(0, 0, 0);
+  myGLCD.fillCircle(Ballx,Bally, 5);
+}
+
+void MoveBall ()
+{
+  Ball ();
+  delay (BallSpeed);
+  ClearBall (); 
+  ResetBall();
+  Ball ();
+  delay (BallSpeed);
+  return;
+
+}
+
+void ResetBall(){
+
+
+  if (Ballx<296 && ResBallx == 0){
+     Ballx=(Ballx+BallRamp);
+     if (Ballx>=295){
+       ResBallx =1;
+     }
+    }
+  if (ResBallx ==1){
+    if (Ballx<=30){
+      ResBallx =0;
+      }
+     Ballx=(Ballx-BallRamp);
+    }
+  if (Bally<230 && ResBally ==0){
+     Bally=(Bally+BallRamp);
+     if (Bally>=229){
+       ResBally =1;
+     }
+    }
+  if (ResBally ==1){
+    if (Bally<=20){
+      ResBally =0;
+      }
+     Bally=(Bally-BallRamp);
+    }
+}
+
+void ScoreScreen ()
+{
+  //Pantalla de Puntaje
+  //myGLCD.clrScr();
+  myGLCD.fillScr(100, 250, 100);
+  myGLCD.setColor(150, 150, 150);
+  myGLCD.setBackColor(150, 150, 150);
+  myGLCD.fillRoundRect(0, 20, 315, 55);
+  myGLCD.setColor(255,255, 255);
+  myGLCD.print("Pong!", CENTER, 32);
+  myGLCD.setBackColor(255, 10, 0);
+  myGLCD.setColor(0, 100, 255);
+  myGLCD.print("J1", 40, 70);
+  myGLCD.setBackColor(255, 10, 0);
+  myGLCD.setColor(0, 100, 255);
+  myGLCD.print("J2", 170, 70);
+  myGLCD.setFont(SevenSegNumFont);
+  myGLCD.printNumI(contadorJ1, 50, 90);
+  myGLCD.printNumI(contadorJ2, 185, 90);
+  myGLCD.setFont(BigFont);
+}
+
+
+void WinnerScreenJ1 ()
+{  
+// Winner Screen J1
+//myGLCD.clrScr();
+  myGLCD.fillScr(100, 250, 100);
+  myGLCD.setColor(150, 150, 150);
+  myGLCD.setBackColor(150, 150, 150);
+  myGLCD.fillRoundRect(0, 20, 315, 55);
+  myGLCD.setColor(255,255, 255);
+  myGLCD.print("Pong!", CENTER, 32);
+  myGLCD.setBackColor(255, 10, 255);
+  myGLCD.setColor(0, 100, 255);
+  myGLCD.print("J1", 40, 70);
+  myGLCD.setBackColor(255, 10, 0);
+  myGLCD.setColor(0, 100, 255);
+  myGLCD.print("J2", 170, 70);
+  myGLCD.setFont(SevenSegNumFont);
+  myGLCD.setBackColor(255, 10, 255);
+  myGLCD.setColor(0, 100, 255);
+  myGLCD.printNumI(contadorJ1, 50, 90);
+  myGLCD.setBackColor(255, 10, 0);
+  myGLCD.setColor(0, 100, 255);
+  myGLCD.printNumI(contadorJ2, 185, 90);
+  myGLCD.setFont(BigFont);
+  myGLCD.setColor(255, 0, 0);
+  myGLCD.fillRoundRect(30, 160, 300,220);  
+  myGLCD.setBackColor(255, 10, 0);
+  myGLCD.setColor(0, 100, 255);
+  myGLCD.print("Winner!!!!!!!", CENTER, 200);
+  myGLCD.setFont(SmallFont);
+  myGLCD.setBackColor(0, 10, 255);
+  myGLCD.setColor(255, 100, 0);
+  myGLCD.print("Click any key to retry!", CENTER, 145);
+  myGLCD.setFont(BigFont);  
+}
+
+
+
+void WinnerScreenJ2 ()
+{
+// Winner Screen J2
+//myGLCD.clrScr();
+  myGLCD.fillScr(100, 250, 100);
+  myGLCD.setColor(150, 150, 150);
+  myGLCD.setBackColor(150, 150, 150);
+  myGLCD.fillRoundRect(0, 20, 315, 55);
+  myGLCD.setColor(255,255, 255);
+  myGLCD.print("Pong!", CENTER, 32);
+  myGLCD.setBackColor(255, 10, 0);
+  myGLCD.setColor(0, 100, 255);
+  myGLCD.print("J1", 40, 70);
+  myGLCD.setBackColor(255, 10, 255);
+  myGLCD.setColor(0, 100, 255);
+  myGLCD.print("J2", 170, 70);
+  myGLCD.setBackColor(255, 10, 0);
+  myGLCD.setFont(SevenSegNumFont);
+  myGLCD.setBackColor(255, 10, 0);
+  myGLCD.setColor(0, 100, 255);
+  myGLCD.printNumI(contadorJ1, 50, 90);
+  myGLCD.setBackColor(255, 10, 255);
+  myGLCD.setColor(0, 100, 255);
+  myGLCD.printNumI(contadorJ2, 185, 90);
+  myGLCD.setFont(BigFont);
+  myGLCD.setColor(255, 0, 0);
+  myGLCD.fillRoundRect(30, 160, 300,220);  
+  myGLCD.setBackColor(255, 10, 0);
+  myGLCD.setColor(0, 100, 255);
+  myGLCD.print("Winner!!!!!!!", CENTER, 200);
+  myGLCD.setFont(SmallFont);
+  myGLCD.setBackColor(0, 10, 255);
+  myGLCD.setColor(255, 100, 0);
+  myGLCD.print("Click any key to retry!", CENTER, 145);
+  myGLCD.setFont(BigFont);
+}
---
+++ ProyectoFinal/musica/musica.ino
@@ -0,0 +1,320 @@
+
+/* 
+  Never Gonna Give you Up
+  Connect a piezo buzzer or speaker to pin 11 or select a new pin.
+  More songs available at https://github.com/robsoncouto/arduino-songs                                            
+                                              
+                                              Robson Couto, 2019
+*/
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+#define REST      0
+
+//Cableadoooo
+//Pata positica del buzzer a 3.3v de la tiva c.
+//Pata negativa del buzzer a colector del buzzer
+//Entre las patas postiva y negativa del buzzer colocar una resistencia de 470 ohms
+//De la pata base del transistor a una resistencia de 470 ohm y de la resistencia al pin PF_2 de la tiva c
+//De la pata emisor del transistor a tierra de la tiva c
+// change this to make the song slower or faster
+
+int tempo = 114;
+
+// change this to whichever pin you want to use
+int buzzer = PF_2;
+
+// Declare Push Button
+
+int button1 = PC_5;
+int button2 = PF_3;
+int button3 = PB_3;
+int button4 = PC_4;
+
+
+// notes of the moledy followed by the duration.
+// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
+// !!negative numbers are used to represent dotted notes,
+// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
+int melody[] = {
+
+  // Never Gonna Give You Up - Rick Astley
+  // Score available at https://musescore.com/chlorondria_5/never-gonna-give-you-up_alto-sax
+  // Arranged by Chlorondria
+
+  NOTE_D5,-4, NOTE_E5,-4, NOTE_A4,4, //1
+  NOTE_E5,-4, NOTE_FS5,-4, NOTE_A5,16, NOTE_G5,16, NOTE_FS5,8,
+  NOTE_D5,-4, NOTE_E5,-4, NOTE_A4,2,
+  NOTE_A4,16, NOTE_A4,16, NOTE_B4,16, NOTE_D5,8, NOTE_D5,16,
+  NOTE_D5,-4, NOTE_E5,-4, NOTE_A4,4, //repeat from 1
+  NOTE_E5,-4, NOTE_FS5,-4, NOTE_A5,16, NOTE_G5,16, NOTE_FS5,8,
+  NOTE_D5,-4, NOTE_E5,-4, NOTE_A4,2,
+  NOTE_A4,16, NOTE_A4,16, NOTE_B4,16, NOTE_D5,8, NOTE_D5,16,
+  REST,4, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_D5,8, NOTE_E5,8, NOTE_CS5,-8,
+  NOTE_B4,16, NOTE_A4,2, REST,4, 
+
+  REST,8, NOTE_B4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,4, NOTE_A4,8, //7
+  NOTE_A5,8, REST,8, NOTE_A5,8, NOTE_E5,-4, REST,4, 
+  NOTE_B4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8, NOTE_D5,8, NOTE_E5,8, REST,8,
+  REST,8, NOTE_CS5,8, NOTE_B4,8, NOTE_A4,-4, REST,4,
+  REST,8, NOTE_B4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8, NOTE_A4,4,
+  NOTE_E5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,4, REST,4,
+   
+  NOTE_D5,2, NOTE_E5,8, NOTE_FS5,8, NOTE_D5,8, //13
+  NOTE_E5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,4, NOTE_A4,4,
+  REST,2, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8,
+  REST,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_FS5,-8, NOTE_FS5,-8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+
+  NOTE_E5,-8, NOTE_E5,-8, NOTE_D5,-8, NOTE_CS5,16, NOTE_B4,-8, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16, //18
+  NOTE_D5,4, NOTE_E5,8, NOTE_CS5,-8, NOTE_B4,16, NOTE_A4,8, NOTE_A4,8, NOTE_A4,8, 
+  NOTE_E5,4, NOTE_D5,2, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_FS5,-8, NOTE_FS5,-8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_A5,4, NOTE_CS5,8, NOTE_D5,-8, NOTE_CS5,16, NOTE_B4,8, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+
+  NOTE_D5,4, NOTE_E5,8, NOTE_CS5,-8, NOTE_B4,16, NOTE_A4,4, NOTE_A4,8,  //23
+  NOTE_E5,4, NOTE_D5,2, REST,4,
+  REST,8, NOTE_B4,8, NOTE_D5,8, NOTE_B4,8, NOTE_D5,8, NOTE_E5,4, REST,8,
+  REST,8, NOTE_CS5,8, NOTE_B4,8, NOTE_A4,-4, REST,4,
+  REST,8, NOTE_B4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8, NOTE_A4,4,
+  REST,8, NOTE_A5,8, NOTE_A5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8, NOTE_D5,8,
+  
+  REST,8, NOTE_A4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8, //29
+  REST,8, NOTE_CS5,8, NOTE_B4,8, NOTE_A4,-4, REST,4,
+  NOTE_B4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8, NOTE_A4,4, REST,8,
+  REST,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,4, NOTE_E5,-4, 
+  NOTE_D5,2, NOTE_D5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,4, 
+  NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8, NOTE_A4,8, NOTE_A4,4,
+
+  REST,-4, NOTE_A4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8, //35
+  REST,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_FS5,-8, NOTE_FS5,-8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_E5,-8, NOTE_E5,-8, NOTE_D5,-8, NOTE_CS5,16, NOTE_B4,8, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_D5,4, NOTE_E5,8, NOTE_CS5,-8, NOTE_B4,16, NOTE_A4,4, NOTE_A4,8, 
+
+   NOTE_E5,4, NOTE_D5,2, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16, //40
+  NOTE_FS5,-8, NOTE_FS5,-8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_A5,4, NOTE_CS5,8, NOTE_D5,-8, NOTE_CS5,16, NOTE_B4,8, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_D5,4, NOTE_E5,8, NOTE_CS5,-8, NOTE_B4,16, NOTE_A4,4, NOTE_A4,8,  
+  NOTE_E5,4, NOTE_D5,2, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+   
+  NOTE_FS5,-8, NOTE_FS5,-8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16, //45
+  NOTE_A5,4, NOTE_CS5,8, NOTE_D5,-8, NOTE_CS5,16, NOTE_B4,8, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_D5,4, NOTE_E5,8, NOTE_CS5,-8, NOTE_B4,16, NOTE_A4,4, NOTE_A4,8,  
+  NOTE_E5,4, NOTE_D5,2, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_FS5,-8, NOTE_FS5,-8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16, //45
+  
+  NOTE_A5,4, NOTE_CS5,8, NOTE_D5,-8, NOTE_CS5,16, NOTE_B4,8, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_D5,4, NOTE_E5,8, NOTE_CS5,-8, NOTE_B4,16, NOTE_A4,4, NOTE_A4,8, 
+
+  NOTE_E5,4, NOTE_D5,2, REST,4
+};
+
+// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+// there are two values per note (pitch and duration), so for each note there are four bytes
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+
+// this calculates the duration of a whole note in ms
+int wholenote = (60000 * 4) / tempo;
+
+int divider = 0, noteDuration = 0;
+
+int hourpic = 0;
+
+
+byte SendData = 0b00000000;
+
+
+void setup() {
+//BUTTON
+  
+  Serial2.begin(9600);
+  
+
+  pinMode(button1, INPUT_PULLUP);
+  pinMode(button2, INPUT_PULLUP);
+  pinMode(button3, INPUT_PULLUP);
+  pinMode(button4, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(button1), buttonFunction1, RISING  );
+  attachInterrupt(digitalPinToInterrupt(button2), buttonFunction2, RISING  );
+  attachInterrupt(digitalPinToInterrupt(button3), buttonFunction3, RISING  );
+  attachInterrupt(digitalPinToInterrupt(button4), buttonFunction4, RISING  );
+
+}
+
+void buttonFunction1 (){
+  int data=1;
+  bitWrite(SendData,1,data);
+  delay(10);
+  if (Serial2.available()>0)
+  {
+    hourpic=Serial.read();
+    Serial2.write(SendData);
+  }
+  delay(1);
+  data = 0;
+  bitWrite(SendData,1,data);
+  /*
+  if (Serial2.available()>0)
+  {
+    hourpic=Serial.read();
+    Serial2.write(SendData);
+  }
+  */
+  }
+
+  void buttonFunction2 (){
+    int data=1;
+  bitWrite(SendData,2,data);
+  delay(10);
+  if (Serial2.available()>0)
+  {
+    hourpic=Serial.read();
+    Serial2.write(SendData);
+  }
+  delay(10);
+  data = 0;
+  bitWrite(SendData,2,data);
+  }
+
+  void buttonFunction3 (){
+    int data=1;
+  bitWrite(SendData,3,data);
+  delay(10);
+  if (Serial2.available()>0)
+  {
+    hourpic=Serial.read();
+    Serial2.write(SendData);
+  }
+  delay(10);
+  data = 0;
+  bitWrite(SendData,3,data);
+  }
+
+
+  void buttonFunction4 (){
+    int data=1;
+  bitWrite(SendData,4,data);
+  delay(10);
+  if (Serial2.available()>0)
+  {
+    hourpic=Serial.read();
+    Serial2.write(SendData);
+  }
+  delay(10);
+  data = 0;
+  bitWrite(SendData,4,data);
+  }
+
+void loop() {
+   // iterate over the notes of the melody.
+  // Remember, the array is twice the number of notes (notes + durations)
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+
+    // calculates the duration of each note
+    divider = melody[thisNote + 1];
+    if (divider > 0) {
+      // regular note, just proceed
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      // dotted notes are represented with negative durations!!
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5; // increases the duration in half for dotted notes
+    }
+
+    // we only play the note for 90% of the duration, leaving 10% as a pause
+    tone(buzzer, melody[thisNote], noteDuration * 0.9);
+
+    // Wait for the specief duration before playing the next note.
+    delay(noteDuration);
+
+    // stop the waveform generation before the next note.
+    noTone(buzzer);
+  }
+}
